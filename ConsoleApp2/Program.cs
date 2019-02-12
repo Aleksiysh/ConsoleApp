0ded930dca0654e45b95996af9a543e8934f6929
Aleksey <aleks-sh@mail.ru>
2019-02-12 20:29:25 +0300

    2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -16,16 +16,21 @@ namespace Recognizer
             //PrintArray(xy);
             //xy = GetTranponMatrix(xy);
             //PrintArray(xy);
-            var g1 = SobelFilterTask.CreatExpandArr(g);
-            PrintArray(g);
-            PrintArray(g1);
+            //var g1 = SobelFilterTask.CreatExpandArr(g);
+            //var arr1 = SobelFilter1(g, sx);
+            //var arr1 = SobelFilter1(new double[,] { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } }, new double[,]{ { 2} });
 
 
-            var arr = Recognizer.SobelFilterTask.SobelFilter(sx, sx);
+
+            PrintArray(SobelFilterTask.SobelFilter(g, sx));
             
+
+
+            
+
         }
 
-       
+
 
 
         static void PrintLine(double[,] arr)
@@ -38,6 +43,61 @@ namespace Recognizer
                 }
             }
         }
+
+        public static double[,] SobelFilter(double[,] g, double[,] sx)
+        {
+            var width = g.GetLength(0);
+            var height = g.GetLength(1);
+            var result = new double[width, height];
+            var lenSx = sx.GetLength(0);
+            for (int x = 1; x < width - 1; x++)
+            {
+                for (int y = 1; y < height - 1; y++)
+                {
+                    // Вместо этого кода должно быть поэлементное умножение матриц sx и полученной из неё sy на окрестность точки (x, y)
+                    // Такая операция ещё называется свёрткой (Сonvolution)
+                    var gx = 0.0;
+                    var gy = 0.0;
+                    for (int k = 0; k < lenSx ; k++)
+                    {
+                        for (int l = 0; l < lenSx; l++)
+                        {
+                            gx += sx[l-(int)(lenSx/2), k-(int)(lenSx/2)];
+                            gy += sx[k - (int)(lenSx / 2), l-(int)(lenSx / 2)];
+                        }
+                    }
+                    result[x, y] = Math.Sqrt(gx * gx + gy * gy);
+                    //var gx =
+                    //    -g[x - 1, y - 1] - 2 * g[x, y - 1] - g[x + 1, y - 1]
+                    //    + g[x - 1, y + 1] + 2 * g[x, y + 1] + g[x + 1, y + 1];
+                    //var gy =
+                    //    -g[x - 1, y - 1] - 2 * g[x - 1, y] - g[x - 1, y + 1]
+                    //    + g[x + 1, y - 1] + 2 * g[x + 1, y] + g[x + 1, y + 1];
+                    //result[x, y] = Math.Sqrt(gx * gx + gy * gy);
+                }
+            }
+
+            return result;
+        }
+        public static double[,] GetCutArr(double[,] arr)
+        {
+            var result = new double[arr.GetLength(0) - 2, arr.GetLength(1) - 2];
+
+            for (int i = 1; i < arr.GetLength(0)-1; i++)
+            {
+                for (int j = 1; j < arr.GetLength(1)-1; j++)
+                {
+                    result[i - 1, j - 1] = arr[i, j];
+                }
+            }
+            return result;
+        }
+
+
+
+
+
+
         static double[,] GetTranponMatrix(double[,] array)
         {
             var result = new double[array.GetLength(1), array.GetLength(0)];
@@ -57,7 +117,7 @@ namespace Recognizer
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    Console.Write(array[i, j] + "\t");
+                    Console.Write("{0:0.###}\t", array[i, j]);
                 }
                 Console.WriteLine();
             }
@@ -65,3 +125,15 @@ namespace Recognizer
         }
     }
 }
+//Error on: Image 3x3
+//sx:
+//1	2	1	
+//0	0	0	
+//-1	-2	-1	
+
+//Image pixels:
+//1	1	1	
+//1	1	0	
+//1	0	0	
+ 
+//Image[0, 1] should be 0, but was 1.4142135623731
--- ConsoleApp2/SobelFilter.cs
+++ ConsoleApp2/SobelFilter.cs
@@ -4,27 +4,49 @@ namespace Recognizer
 {
     internal static class SobelFilterTask
     {
+
+
         public static double[,] SobelFilter(double[,] g, double[,] sx)
         {
+
+            g = CreatExpandArr(g);
+            var sy = GetTranponMatrix(sx);
             var width = g.GetLength(0);
             var height = g.GetLength(1);
             var result = new double[width, height];
+            var lenSx = sx.GetLength(0);
             for (int x = 1; x < width - 1; x++)
             {
                 for (int y = 1; y < height - 1; y++)
                 {
-                    // Вместо этого кода должно быть поэлементное умножение матриц sx и полученной из неё sy на окрестность точки (x, y)
-                    // Такая операция ещё называется свёрткой (Сonvolution)
-                    var gx =
-                        -g[x - 1, y - 1] - 2 * g[x, y - 1] - g[x + 1, y - 1]
-                        + g[x - 1, y + 1] + 2 * g[x, y + 1] + g[x + 1, y + 1];
-                    var gy =
-                        -g[x - 1, y - 1] - 2 * g[x - 1, y] - g[x - 1, y + 1]
-                        + g[x + 1, y - 1] + 2 * g[x + 1, y] + g[x + 1, y + 1];
+                    var gx = 0.0;
+                    var gy = 0.0;
+                    for (int k = 0; k < lenSx; k++)
+                    {
+                        for (int l = 0; l < lenSx; l++)
+                        {
+                            gx += sx[l, k] * g[x + (l - (int)(lenSx / 2)), y + (k - (int)(lenSx / 2))];
+                            gy += sy[l, k] * g[x + (l - (int)(lenSx / 2)), y + (k - (int)(lenSx / 2))];
+                        }
+                    }
                     result[x, y] = Math.Sqrt(gx * gx + gy * gy);
                 }
             }
 
+            return GetCutArr(result);
+        }
+
+        public static double[,] GetCutArr(double[,] arr)
+        {
+            var result = new double[arr.GetLength(0) - 2, arr.GetLength(1) - 2];
+
+            for (int i = 1; i < arr.GetLength(0) - 1; i++)
+            {
+                for (int j = 1; j < arr.GetLength(1) - 1; j++)
+                {
+                    result[i - 1, j - 1] = arr[i, j];
+                }
+            }
             return result;
         }
 
@@ -55,8 +77,45 @@ namespace Recognizer
             }
             return result;
         }
+
+        static double[,] GetTranponMatrix(double[,] array)
+        {
+            var result = new double[array.GetLength(1), array.GetLength(0)];
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                for (int j = 0; j < array.GetLength(0); j++)
+                {
+                    result[i, j] = array[j, i];
+                }
+            }
+            return result;
+        }
     }
 }
+//public static double[,] SobelFilter1(double[,] g, double[,] sx)
+//{
+//    var width = g.GetLength(0);
+//    var height = g.GetLength(1);
+//    var result = new double[width, height];
+//    for (int x = 1; x < width - 1; x++)
+//    {
+//        for (int y = 1; y < height - 1; y++)
+//        {
+//            // Вместо этого кода должно быть поэлементное умножение матриц sx и полученной из неё sy на окрестность точки (x, y)
+//            // Такая операция ещё называется свёрткой (Сonvolution)
+//            var gx =
+//                -g[x - 1, y - 1] - 2 * g[x, y - 1] - g[x + 1, y - 1]
+//                + g[x - 1, y + 1] + 2 * g[x, y + 1] + g[x + 1, y + 1];
+//            var gy =
+//                -g[x - 1, y - 1] - 2 * g[x - 1, y] - g[x - 1, y + 1]
+//                + g[x + 1, y - 1] + 2 * g[x + 1, y] + g[x + 1, y + 1];
+//            result[x, y] = Math.Sqrt(gx * gx + gy * gy);
+//        }
+//    }
+
+//    return result;
+//}
+
 /* 
 Разберитесь, как работает нижеследующий код (называемый фильтрацией Собеля), 
 и какое отношение к нему имеют эти матрицы:
